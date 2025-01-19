4e90d4a40b4a765fafecd1a4f880dec23142cbfe
leonard <leonard@renoud.ca>
2025-01-18 19:55:22 -0500

    fix(creation): commit de creation dans la mauvaise branche

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,7 +4,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AppComponent } from '@app/pages/app/app.component';
-import { CreatePageComponent } from '@app/pages/creation-page/create-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -18,7 +17,6 @@ if (environment.production) {
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
-    { path: 'create', component: CreatePageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: '**', redirectTo: '/home' },
