b5da1ed6e93c6dde4d9e9d600c25dcdfcc9d156f
ArnaudGrd <arnaud.grandisson@polymtl.ca>
2025-01-20 12:46:37 -0500

    refactor(vue-edition): Modification du code pour utiliser les nouveaux components

--- client/src/app/pages/edit-page/edit-page.component.ts
+++ client/src/app/pages/edit-page/edit-page.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
+import { EditFormComponent } from '@app/components/edit-form/edit-form.component';
+import { EditItemAreaComponent } from '@app/components/edit-item-area/edit-item-area.component';
+import { EditMapAreaComponent } from '@app/components/edit-map-area/edit-map-area.component';
 
 @Component({
     selector: 'app-edit-page',
     templateUrl: './edit-page.component.html',
     styleUrls: ['./edit-page.component.scss'],
+    imports: [EditFormComponent, EditItemAreaComponent, EditMapAreaComponent],
 })
-export class EditPageComponent {
-    mapName = '';
-    hasModeCTF = false;
-    mapSize = 10;
-    mapDescription = '';
-    isMapSaved = false;
-    selectedGrid = 'TilesGrid';
-    onSubmit() {
-        this.isMapSaved = true;
-    }
-    showGrid(gridId: string) {
-        this.selectedGrid = gridId;
-    }
-}
+export class EditPageComponent {}
