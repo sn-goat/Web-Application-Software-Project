593316c600fec96a3735571e14f4b824da0d8d01
scriptmaze <hossamfelfel200@Yahoo.com>
2025-01-20 13:01:12 -0500

    updated routing and dependencies

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,6 +7,7 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -19,6 +20,7 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'admin', component: AdminPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
