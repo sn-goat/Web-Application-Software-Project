cddd63d7e8542dcf092b5d1b8d8114efca64b57b
iyers16 <shanmukh.iyer@gmail.com>
2025-01-24 02:53:11 -0500

    feat(cell): right click to delete tile done

--- client/src/app/components/board-cell/board-cell.component.ts
+++ client/src/app/components/board-cell/board-cell.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnDestroy, HostListener } from '@angular/core';
-import { BoardCell } from '@app/interfaces/board/board-cell';
-import { EditToolMouse } from '@app/classes/edit-tool-mouse/edit-tool-mouse';
-import { Tiles, Items } from '@app/enum/tile';
 import { CommonModule } from '@angular/common';
+import { Component, HostListener, Input, OnDestroy } from '@angular/core';
+import { EditToolMouse } from '@app/classes/edit-tool-mouse/edit-tool-mouse';
+import { Items, Tiles } from '@app/enum/tile';
+import { BoardCell } from '@app/interfaces/board/board-cell';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
@@ -32,6 +32,12 @@ export class BoardCellComponent implements OnDestroy {
         this.updateCell();
     }
 
+    @HostListener('contextmenu', ['$event'])
+    onRightClick(event: MouseEvent) {
+        event.preventDefault();
+        this.revertToWall();
+    }
+
     ngOnDestroy() {
         this.destroy$.next();
         this.destroy$.complete();
@@ -49,5 +55,11 @@ export class BoardCellComponent implements OnDestroy {
         });
         this.destroy$.next();
         this.destroy$.complete();
+    }
+
+    private revertToWall() {
+        this.cell.tile = Tiles.Default;
+        this.imageUrl = './assets/tiles/Wall.png';
+        this.cell.item = Items.NoItem;
     }
 }
