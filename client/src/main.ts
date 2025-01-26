32c47b352753cd00d4d2cf19cc64a7990dcb3e73
leonard <leonard@renoud.ca>
2025-01-26 18:21:30 -0500

    chore(vue-initiale): Enlever les stub et mettre les vrais components

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
-import { Component, enableProdMode } from '@angular/core';
+import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
@@ -9,26 +9,12 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
-import { environment } from './environments/environment';
 
-@Component({
+Component({
     template: `
      <div></div>
     `
 })
-class CreationPageComponentStub {}
-
-@Component({
-    template: `
-     <div></div>
-    `
-})
-class AdminPageComponentStub {}
-
-if (environment.production) {
-    enableProdMode();
-}
-
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -36,8 +22,8 @@ const routes: Routes = [
     { path: 'creation', component: CreatePageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
-    { path: 'creation', component: CreationPageComponentStub  },
-    { path: 'admin', component: AdminPageComponentStub },
+    { path: 'creation', component: CreatePageComponent  },
+    { path: 'admin', component: AdminPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
