bc713df85f11925783249613495b55648e07a2b8
Hubert Khouzam <hub.khouzam@gmail.com>
2025-01-26 21:52:50 -0500

    fix - EditPageComponent and FormDialogComponent tests

--- client/src/app/components/form-dialog/form-dialog.component.spec.ts
+++ client/src/app/components/form-dialog/form-dialog.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormDialogComponent } from './form-dialog.component';
 
 describe('FormDialogComponent', () => {
@@ -8,7 +9,19 @@ describe('FormDialogComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [FormDialogComponent],
+            imports: [FormDialogComponent, BrowserAnimationsModule],
+            providers: [
+                {
+                    provide: MatDialogRef,
+                    useValue: {
+                        close: jasmine.createSpy('close'),
+                    },
+                },
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {},
+                },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(FormDialogComponent);
--- client/src/app/pages/edit-page/edit-page.component.spec.ts
+++ client/src/app/pages/edit-page/edit-page.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { EditPageComponent } from './edit-page.component';
 
 describe('EditPageComponent', () => {
     let component: EditPageComponent;
     let fixture: ComponentFixture<EditPageComponent>;
 
+    const queryParamsSubject = new BehaviorSubject({
+        name: 'Test Name',
+        size: '20',
+        description: 'Test Description',
+    });
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [EditPageComponent],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        queryParams: queryParamsSubject.asObservable(),
+                    },
+                },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(EditPageComponent);
