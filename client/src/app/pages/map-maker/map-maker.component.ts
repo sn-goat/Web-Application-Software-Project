10ab4b3598c10058e72bfa542cb120e9950de346
leonard <leonard@renoud.ca>
2025-02-03 13:04:18 -0500

    style(map-maker): Modifier le design de map-maker

--- client/src/app/pages/map-maker/map-maker.component.ts
+++ client/src/app/pages/map-maker/map-maker.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ActivatedRoute } from '@angular/router';
 import { BoardGameComponent } from '@app/components/board-game/board-game.component';
 import { EditItemAreaComponent } from '@app/components/edit-item-area/edit-item-area.component';
 
 @Component({
     selector: 'app-map-maker',
-    imports: [MatSidenavModule, MatGridListModule, MatFormFieldModule, MatIconModule, BoardGameComponent, EditItemAreaComponent],
+    imports: [MatSidenavModule, MatGridListModule, MatFormFieldModule, MatIconModule, MatMenuModule, MatButtonModule, BoardGameComponent, EditItemAreaComponent],
     templateUrl: './map-maker.component.html',
-    styleUrl: './map-maker.component.scss',
+    styleUrls: ['./map-maker.component.scss'],
 })
 export class MapMakerComponent implements OnInit {
     data: { name: string; size: number; description: string } = { name: '', size: 10, description: '' };
