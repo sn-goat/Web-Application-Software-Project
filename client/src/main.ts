11798e818af37582ab028a80a752c512f30942c1
iyers16 <shanmukh.iyer@gmail.com>
2025-02-05 20:04:34 -0500

    removed unused page

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,24 +2,21 @@ import { provideHttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
+import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { AppComponent } from '@app/pages/app/app.component';
 import { CreatePageComponent } from '@app/pages/create-page/create-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
-// import { EditPageComponent } from '@app/pages/edit-page/edit-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { MapMakerComponent } from '@app/pages/map-maker/map-maker.component';
-
-
+import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 
 Component({
     template: `
      <div></div>
-    `
-})
+    `,
+});
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -28,11 +25,17 @@ const routes: Routes = [
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: 'edit', component: MapMakerComponent },
-    { path: 'creation', component: CreatePageComponent  },
+    { path: 'creation', component: CreatePageComponent },
     { path: 'admin', component: AdminPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient(), provideRouter(routes, withHashLocation()), provideAnimations(), provideAnimationsAsync(), provideAnimationsAsync()],
-})
+    providers: [
+        provideHttpClient(),
+        provideRouter(routes, withHashLocation()),
+        provideAnimations(),
+        provideAnimationsAsync(),
+        provideAnimationsAsync(),
+    ],
+});
