0e084f1ef5a3ec610d4c5d6ec78c2297c5776e82
ArnaudGrd <arnaud.grandisson@polymtl.ca>
2025-02-07 18:04:39 -0500

    Correction de la reload page

--- client/src/app/services/map.service.ts
+++ client/src/app/services/map.service.ts
@@ -6,9 +6,18 @@ import { BehaviorSubject } from 'rxjs';
     providedIn: 'root',
 })
 export class MapService {
-    private mapData = new BehaviorSubject<Board>({} as Board);
+    private readonly storageKey = 'mapData';
+    private mapData: BehaviorSubject<Board>;
+
+    constructor() {
+        const savedData = localStorage.getItem(this.storageKey);
+        const initialData = savedData ? JSON.parse(savedData) : ({} as Board);
+        this.mapData = new BehaviorSubject<Board>(initialData);
+    }
+
     setMapData(data: Board): void {
         this.mapData.next(data);
+        localStorage.setItem(this.storageKey, JSON.stringify(data));
     }
 
     getMapData(): BehaviorSubject<Board> {
