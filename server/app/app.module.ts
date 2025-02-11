817b6d84b504e753d9f5e17a74ea4842cc6514fc
iyers16 <shanmukh.iyer@gmail.com>
2025-02-10 20:18:45 -0500

    changed server imports to exclude boilerplate deletions

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -1,16 +1,9 @@
-import { CourseController } from '@app/controllers/course/course.controller';
-import { DateController } from '@app/controllers/date/date.controller';
-import { ExampleController } from '@app/controllers/example/example.controller';
 import { ChatGateway } from '@app/gateways/chat/chat.gateway';
 import { Board, boardSchema } from '@app/model/database/board';
-import { CourseService } from '@app/services/course/course.service';
-import { DateService } from '@app/services/date/date.service';
-import { ExampleService } from '@app/services/example/example.service';
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BoardController } from './controllers/board/board.controller';
-import { Course, courseSchema } from './model/database/course';
 import { BoardService } from './services/board/board.service';
 
 @Module({
@@ -25,10 +18,9 @@ import { BoardService } from './services/board/board.service';
         }),
         MongooseModule.forFeature([
             { name: Board.name, schema: boardSchema },
-            { name: Course.name, schema: courseSchema },
         ]),
     ],
-    controllers: [CourseController, DateController, ExampleController, BoardController],
-    providers: [ChatGateway, CourseService, DateService, ExampleService, BoardService, Logger],
+    controllers: [BoardController],
+    providers: [ChatGateway, BoardService, Logger],
 })
 export class AppModule {}
