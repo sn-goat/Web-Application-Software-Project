61cc1c8fa88fd92f81d3511fc489df4eed1f2513
iyers16 <shanmukh.iyer@gmail.com>
2025-02-10 19:49:06 -0500

    amended cell from dev

--- client/src/app/components/edit/board-cell/board-cell.component.ts
+++ client/src/app/components/edit/board-cell/board-cell.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { ASSETS_DESCRIPTION } from '@app/constants/descriptions';
 import { DEFAULT_PATH_ITEMS, DEFAULT_PATH_TILES } from '@app/constants/path';
 import { ToolSelectionService } from '@app/services/code/tool-selection.service';
 import { Cell } from '@common/board';
+import { Item } from '@common/enums';
 
 @Component({
     selector: 'app-board-cell',
@@ -15,6 +17,7 @@ export class BoardCellComponent {
     readonly srcTiles = DEFAULT_PATH_TILES;
     readonly srcItem = DEFAULT_PATH_ITEMS;
     readonly fileType = '.png';
+    showTooltip = false;
 
     constructor(private toolSelection: ToolSelectionService) {}
 
@@ -27,5 +30,9 @@ export class BoardCellComponent {
 
     onDragOver(event: DragEvent) {
         event.preventDefault();
+    }
+
+    getItemDescription(type: Item): string {
+        return ASSETS_DESCRIPTION.get(type) ?? '';
     }
 }
