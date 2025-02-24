a766304af129af9eb073e89a246bc84e5982ef64
iyers16 <shanmukh.iyer@gmail.com>
2025-02-24 13:14:20 -0500

    created player model

---
+++ common/player.ts
@@ -0,0 +1,11 @@
+export interface Player {
+    id: string;
+    username: string;
+    avatar: string;
+    life: number;
+    attack: number;
+    defense: number;
+    rapidity: number;
+    attackDice: string;
+    defenseDice: string;
+}
