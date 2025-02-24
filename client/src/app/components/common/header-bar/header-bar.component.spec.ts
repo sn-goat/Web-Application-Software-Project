9b4fe1e1c4cb82bf0b19912f643f7d5bcf295b3e
leonard <leonard@renoud.ca>
2025-02-24 17:10:07 -0500

    test(header-bar): Ajouter des tests

--- client/src/app/components/common/header-bar/header-bar.component.spec.ts
+++ client/src/app/components/common/header-bar/header-bar.component.spec.ts
@@ -1,22 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 import { HeaderBarComponent } from './header-bar.component';
 
 describe('HeaderBarComponent', () => {
     let component: HeaderBarComponent;
     let fixture: ComponentFixture<HeaderBarComponent>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [HeaderBarComponent],
+    beforeEach(() => {
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+        TestBed.configureTestingModule({
+            imports: [HeaderBarComponent,MatIconModule, MatButtonModule, MatToolbarModule],
+            providers: [{ provide: Router, useValue: routerSpy }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(HeaderBarComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should navigate to backUrl when getBack is called', () => {
+        component.backUrl = 'home';
+        component.getBack();
+        expect(routerSpy.navigate).toHaveBeenCalledWith(['/home']);
     });
 });
