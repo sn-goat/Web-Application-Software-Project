8a9267d7614d90b5be15a2435b454163ce22f8ac
Sn-goat <sergile-junior.domguia-nyouvop@polymtl.ca>
2025-03-01 02:35:20 -0500

    minor modif

--- client/src/app/constants/path.ts
+++ client/src/app/constants/path.ts
@@ -1,11 +1,6 @@
 export const DEFAULT_ASSET_PATH = './assets/';
-
 export const DEFAULT_PATH_TILES = DEFAULT_ASSET_PATH + 'tiles/';
-
 export const DEFAULT_PATH_ITEMS = DEFAULT_ASSET_PATH + 'items/';
-
 export const DEFAULT_PATH_AVATARS = DEFAULT_ASSET_PATH + 'portraits/portrait';
-
 export const DEFAULT_PATH_DICE = DEFAULT_ASSET_PATH + 'dice/';
-
 export const DEFAULT_FILE_TYPE = '.png';
