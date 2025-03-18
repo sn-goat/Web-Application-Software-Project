dc3b3b088365a274d71338f22df65ebb65e855b7
Hubert Khouzam <hub.khouzam@gmail.com>
2025-03-18 23:16:59 +0000

    Ajout: Auto attack et Timer plus court

--- server/app/gateways/game/game.gateway.constants.ts
+++ server/app/gateways/game/game.gateway.constants.ts
@@ -1,6 +1,7 @@
 export const THREE_SECONDS_IN_MS = 3000;
 export const TURN_DURATION_IN_S = 30;
 export const FIGHT_TURN_DURATION_IN_S = 5;
+export const FIGHT_TURN_DURATION_NO_FLEE_IN_S = 3;
 export const MOVEMENT_TIMEOUT_IN_MS = 150;
 export const RANDOM_SORT_OFFSET = 0.5;
 
