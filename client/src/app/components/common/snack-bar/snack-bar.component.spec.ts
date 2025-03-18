2e4935426b036edecde91de13692ea5bbc3c505b
scriptmaze <hossamfelfel200@Yahoo.com>
2025-03-17 21:04:18 -0400

    test(snackbar): add provider for MAT_SNACK_BAR_DATA in SnackbarComponent tests

--- client/src/app/components/common/snack-bar/snack-bar.component.spec.ts
+++ client/src/app/components/common/snack-bar/snack-bar.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SnackbarComponent } from './snack-bar.component';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 describe('SnackbarComponent', () => {
     let component: SnackbarComponent;
@@ -9,6 +9,7 @@ describe('SnackbarComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [SnackbarComponent],
+            providers: [{ provide: MAT_SNACK_BAR_DATA, useValue: { message: 'Test Message' } }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(SnackbarComponent);
