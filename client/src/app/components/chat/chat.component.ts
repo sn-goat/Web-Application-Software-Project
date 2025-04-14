d26214e15d5a36eef7439f5f54f597de17dff7d4
iyers16 <shanmukh.iyer@gmail.com>
2025-04-14 11:08:29 -0400

    fix(chat): persistence issue fixed by unsubscribing to the sub object instead of the field

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { ChatService } from '@app/services/chat/chat.service';
 import { PlayerService } from '@app/services/player/player.service';
 import { IPlayer } from '@common/player';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-chat',
@@ -17,23 +18,23 @@ export class ChatComponent implements AfterViewChecked, OnDestroy {
     newMessage: string = '';
     myPlayer: IPlayer | null = null;
     private previousMessageCount: number = 0;
+    private subscriptions: Subscription[] = [];
 
     constructor(
         private readonly playerService: PlayerService,
         readonly chatService: ChatService,
     ) {
-        this.playerService.myPlayer.subscribe((player) => {
-            this.myPlayer = player;
-        });
+        this.subscriptions.push(
+            this.playerService.myPlayer.subscribe((player) => {
+                this.myPlayer = player;
+            })
+        );
     }
 
     ngOnDestroy(): void {
-        this.chatService.clearChatHistory();
-        this.previousMessageCount = 0;
-        this.newMessage = '';
-        this.myPlayer = null;
-        // this.playerService.myPlayer.unsubscribe();
+        this.subscriptions.forEach(s => s.unsubscribe());
     }
+
 
     ngAfterViewChecked(): void {
         const currentMessages = this.chatService.chatHistory();
