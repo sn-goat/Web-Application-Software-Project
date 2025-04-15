ed507524b8396884907ad5c5e1c73298c9304675
Shanmukh Iyer <shanmukh.iyer@gmail.com>
2025-04-15 01:30:42 -0400

    Deployment: Rectification du nouveau url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-15-223-119-251.ca-central-1.compute.amazonaws.com:3000',
+    serverUrl: 'http://ec2-35-183-98-64.ca-central-1.compute.amazonaws.com:3000',
 };
